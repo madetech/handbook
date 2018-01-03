acede386990bc7b8f4f17869ccda0ee010bfb39f
Chris Blackburn <chris@madetech.com>
2018-01-03 18:59:23 +0000

    Add Lead Engineer role (#95)

---
+++ roles/as_code/lib/shared_expectations/delivery_lead_expectations.rb
@@ -0,0 +1,28 @@
+class DeliveryLeadExpectations < JobSpec::Role::Expectations
+  expected 'be the point of contact for the customer',
+    'We expect our Delivery Leads to naturally position themselves as the day to day point of contact for the customer. A Delivery Lead is expected to gain the trust of the customer such that they’re seen as the first port of call for all customer contacts.'
+
+  expected 'ensure customer expectations are exceeded given the budget available',
+    'We expect our Delivery Leads to understand the financial constraints, as well as the customer objectives for the delivery. A Delivery Lead is accountable for ensuring the customer’s expectations are exceeded for the available budget.'
+
+  expected 'ultimately accountable for the successful delivery of customer engagements',
+    'We expect our Delivery Leads to assume ultimate responsibility for the successful delivery of customer engagements. The performance of the team and the delivery as a whole is understood as the Delivery Lead’s responsibility.'
+
+  expected 'provide accurate and honest reporting of Delivery Healthcheck to Directors',
+    'We expect our Delivery Leads to proactively provide regular reporting to the Directors on the health of their delivery.'
+
+  expected 'ensure that at all times the team is delivering according to the Made Tech Delivery Standards',
+    'We expect our Delivery Leads to be the guardians of the Made Tech Delivery Standards, ensuring the team understands and is proactively practising them. A Delivery Lead is expected to encourage conversation on both more widely evolving the Delivery Standards, and ahead of deciding to follow a different path on their delivery.'
+
+  expected 'ensure at all times that the customer would recommend Made Tech to their peers',
+    'We expect our Delivery Leads to take accountability for ensuring the customer is delighted with the quality of our delivery to the level that they would not hesitate to recommend Made Tech to their peers.'
+
+  expected 'protect the team from influences that may impact their ability to deliver',
+    'We expect our Delivery Leads to demonstrate strong diplomacy when protecting their team from influences that may impact the team’s ability to deliver its objectives and commitments.'
+
+  expected 'ensure the team collaborates, communicates and focuses on what is most important',
+    'We expect our Delivery Leads to ensure the team is actively collaborating and communicating, both between team members and with the customer. A Delivery Lead is expected to ensure the team have a shared understanding of what is most important to the customer.'
+
+  expected 'coach team members and customers, facilitate continuous improvement, and apply the most appropriate agile and lean tools and techniques for their environment',
+    'We expect our Delivery Leads to be an expert in the application of agile and lean practices. A Delivery Lead is expected to understand the most appropriate methods to apply given the situation. A Delivery Lead is expected to coach team members and customers on the application of lean and agile practices.'
+end
