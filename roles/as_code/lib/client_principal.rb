0e1cb96bcc3d9f8b4a9d2465f95f51389b7a5b79
Chris Blackburn <chris@madetech.com>
2018-06-19 17:16:12 +0100

    Add Client Principal expectations (#157)

---
+++ roles/as_code/lib/client_principal.rb
@@ -0,0 +1,27 @@
+JobSpec::Role.definition 'Client Principal' do
+  description <<~DESCRIPTION
+    Client Principals are responsible for maintaining senior relationships within our customer organisations. They understand a customer\'s strategic objectives, and help teams navigate politically tricky situations.
+
+    Client Principals coach and support Lead Engineers and Delivery Managers to meet their role expectations, aiming to ultimately delegate commercial responsibility to the team.
+
+    Client Principals challenge their teams to uphold Made Tech's delivery standards.
+  DESCRIPTION
+
+  expected 'to be the point of escalation',
+    'We expect our Client Principals to position themselves as the customer\'s point of escalation. Where a customer feels they\'ve reached an impasse with their day-to-day contact, Client Principals should be seen as a peer customers can turn to for support.'
+
+  expected 'to keep delivery managers and lead engineers updated',
+    'We expect our Client Principals to keep Delivery Managers and Lead Engineers up to date with the outcome of communication they have with customer teams.'
+
+  expected 'to help navigate organisations',
+    'We expect our Client Principals to understand the objectives and goals of key stakeholders in customer organisations, and to help teams navigate this terrain in support of their delivery.'
+
+  expected 'to coach lead engineers and delivery managers',
+    'We expect our Client Principals to coach and support Lead Engineers and Delivery Managers to meet their role expectations. Client Principals are expected to remain \'hands off\' on a day-to-day basis, supporting the team by providing guidance.'
+
+  expected 'to challenge teams',
+    'We expect our Client Principals to challenge their teams to uphold Made Tech\'s delivery standards.'
+
+  expected 'to provide line management',
+    'We expect our Client Principals to provide line management to the Delivery Managers and Lead Engineers with whom they\'re working with most closely.'
+end
