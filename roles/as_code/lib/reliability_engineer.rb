312699f09e2ca0a9f07c71504c4d8823c1c3ec8d
Luke Morton <lukemorton.dev@gmail.com>
2018-06-25 14:52:44 +0100

    Add Customer Support Rep role to Handbook

--- roles/as_code/lib/reliability_engineer.rb
+++ roles/as_code/lib/reliability_engineer.rb
@@ -1,4 +1,5 @@
 require_relative './shared_expectations/core_engineer_expectations'
+require_relative './shared_expectations/live_services_expectations'
 
 JobSpec::Role.definition 'Reliability Engineer' do
   description <<~DESCRIPTION
@@ -66,20 +67,9 @@ JobSpec::Role.definition 'Reliability Engineer' do
   expected 'to be fully billable by timesheeting 7 hours daily',
     'We expect our Reliability Engineers to ensure they are timesheeting 7 hours daily into harvest. As our current offering means we offer hours to our customers we need to make sure we are working through these hours and providing value to our customers.'
 
-  expected 'to regularly communicate with customers without reminder',
-    'We expect our Reliability Engineers to keep their fellow team members up to date with their progress. A good litmus test for this is everyone on the team can describe what their team mates are up to.'
-
-  expected 'to be a responsible remote worker',
-    'We expect our Reliability Engineers to communicate more than anyone else in the business if they choose to work fully remote. Remote working requires extra attention to be paid to communication channels like email and slack.'
-
-  expected 'to effectively juggle commitments',
-    'We expect our Reliability Engineers to be able to balance the priority of their work. Having an understanding of the severity of work along with expected time to fix is important and it should be used to help prioritise what to work on. Customers expectations need to be set, met and if they cannot be met, be reset early and often.'
-
-  expected 'to provide an outstanding customer service',
-    'We expect our Reliability Engineers to be a delightful person to talk to for our customers. Our conversations should be professional and friendly, we should small talk where possible, use a smiley face once in a while and generally be nice folk.'
-
   expected 'to proactively identify technical improvements and recommend them to customers',
     'We expect our Reliability Engineers to be able to spot areas for improvement in customer codebases such as test suites, dependency upgrades, performance issues detected by monitoring. We also expect them to be able to highlight these issues in a jargon free way and be able to convey the relative urgency of the issues ultimately leading to our customer codebases being improved.'
 
+  include LiveServicesExpectations, as: 'Live Services Expectations'
   include CoreEngineerExpectations, as: 'Core Engineer Expectations'
 end
