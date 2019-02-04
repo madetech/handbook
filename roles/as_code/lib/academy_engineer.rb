ec259e98be75f47df5ec141b2f8233e214658d2b
Craig J. Bass <craig@madetech.co.uk>
2019-02-04 14:16:53 +0000

    Update Academy Engineer expectations inline with v2

--- roles/as_code/lib/academy_engineer.rb
+++ roles/as_code/lib/academy_engineer.rb
@@ -3,14 +3,72 @@ require_relative './shared_expectations/core_engineer_expectations'
 JobSpec::Role.definition 'Academy Engineer' do
   description 'Our software delivery teams are the core of our business. They are usually comprised of a Lead Engineer, a Delivery Manager and one or more Engineers. As an Academy Engineer we expect someone:'
 
-  expected 'to be actively working towards becoming an Engineer',
-    'The Academy Engineer role is a temporary one. Our aim is for most Academy Engineers to move into an Engineer role after completing the 12 week academy programme or latest within 12 months. We will actively work with Academy Engineers to develop career development plans so it is clear on the steps needing to be taken in order to become an Engineer.'
+  expected "To be responsible for the security of devices used for work", 
+    'We expect our engineering team to understand how to secure the devices they use for work including work issued laptops, personal computers, mobile phones, and other electronic devices. We expect invididuals to ensure they adhere to our [security guidelines](https://github.com/madetech/handbook/blob/master/guides/security/protect_the_company.md) upon setting up devices and to ensure that their machines are compliant at all times.'
 
-  expected 'to be in the office',
+  expected "To be productive, in at least one programming language including a range of different tools that achieve the same goal in that language", 
+    'We expect our engineering team to understand that there are multiple tools available to achieve the same task at hand, and to explore these options in at least one language.'
+
+  expected "To continuously practicing TDD, including small incremental refactorings throughout the process of delivering software, with the intent to ensure low cost of change", 
+    'We expect our engineering team to practice TDD discipline to a high standard. This is not something we simply pay lipservice to, we expect it to be a default in every engineer\' day to day'
+
+  expected "To be able to productively pair with colleagues, and to pair when it is appropriate to do so", 
+    'We expect our engineering team to be friendly and lovely people to pair with. All pairing dynamics are different, sometimes you will be pairing with someone with less experience, sometimes you will have the same experience. Adjusting the speed of your work to accomodate for your pair is important and special care must be taken when mentoring/coaching those with less experience. No matter who you are pairing with you must take turns to compromise when your opinions differ.'
+
+  expected "To be in the office", 
     'We expect our Academy Engineers to be working from our office so that they can easily receive help from their colleagues. We have found that being physically present means it\'s much easier to ask colleagues for a quick bit of help. Not only that but it\'s important to build strong relationships with your colleagues and there is nothing like being in the office for doing that. All our other roles are remote friendly and we expect once you have been promoted to Engineer then you will have access to this benefit too.'
 
-  expected 'to be involved in improving the academy program for current and future cohorts',
-    'We expect Academy Engineers to have lots of opinions and feedback on our academy as they will have experienced it first hand. We expect this insight to be put to good use to improve our academy for current and future cohorts.'
+  expected "To request peer reviews",
+    'We expect our engineering team to always have their code peer reviewed at the least by one other person before merging. It\'s the responsibility of the code author to ask their colleagues to review and to merge the code in a timely fashion.'
 
-  include CoreEngineerExpectations, as: 'Core Engineer Expectations'
+  expected "To provide meaningful and considerate peer reviews for others", 
+    'We expect our engineering team to provide feedback that makes code safer, easier to maintain and advances the knowledge of the original author. We expect everyone to negotiate and compromise when making a call on whether feedback should be addressed or not.'
+
+  expected "To be contributing your honest opinion to the team, in a respectful way, even if it conflicts with more senior members", 
+    'We expect our engineering team to work as a team. That means we need our engineers to actively work to combine their knowledge, skill and opinions with that of the whole team.'
+
+  expected "To take an equal share in owning the facilitation of team ceremonies, with a focus on ensuring that they are held regularly and are following any rules that the team has agreed to.", 
+    'We expect our engineering team to ensure team ceremonies such as planning, retrospectives, and standups happen on a regular basis and to be guardians of any norms that have been agreed to by the team.'
+
+  expected "To be working towards achieving the Made Tech Core Skills", 
+    'We expect our Engineers to be honing their craft, practising at every opportunity and consistently making progress towards attaining all the Made Tech Core Skills.'
+
+  expected "To continuously translate user needs into fast, maintainable, automated high-level tests as appropriate", 
+    'We expect our engineering team to write high-level tests, ideally as part of an Acceptance-Test Driven Development discipline.'
+  
+  expected "To be willing and able to pick up new programming languages, technologies and techniques", 
+    'We expect our engineering team to be open to learning new languages and to see it as an opportunity to broaden skills rather than something to be fearful of. We expect our engineers to be curating their generalist skillset to aid in picking up new languages.'
+  
+  expected "To be continuously applying a lean and iterative approach to solving problems", 
+    'We expect our engineering team to embody a lean-mindset, solving problems by delivering early and often. We expect our engineers to actively seek out more effective ways to achieve this.'
+
+  expected "To be practicing continuous integration of code, making frequent small incremental changes to the code base, decoupling deployment from release as appropriate", 
+    'We expect our engineering team to be practicing continuous integration, and to consider decoupling release from deployment, if required and appropriate.'
+
+  expected "To keep the team updated on current work in progress, including any blockers or potential risks, making use of any tooling the team has chosen.", 
+    'We expect our engineering team to keep their fellow team members up to date with their progress. A good litmus test for this is everyone on the team can describe what their team mates are up to.'
+
+  expected "To communicate with customers without reminder.", 
+    'We expect our engineering team to keep their customers up to date with progress. Customers should be invited to standups or at appropriate points in time. A customer should never have to chase us for an update.'
+
+  expected "To be involved in improving software delivery practice at Made Tech",
+    'We expect our engineering team to reflect on our practices and be a proactive voice in suggesting change. We expect you to be reflecting on your own development practices, reflecting on your team and the wider business.'
+
+  expected "To be responsible for releasing working software to real users", 
+    'We expect our engineering team to own the whole process of getting working software in front of real users, this means from writing the code through to deploying and actively seeking feedback.'
+
+  expected "To be producing sensible lightweight release plans with an aim to get the thinnest slice possible in the hands of real users as quickly as possible, involving the team and stakeholders where appropriate.", 
+    'We expect our engineering team to be actively form sensible strategies to get thin slices released to real users, and to have alignment on this strategy with the customer and with the team.'
+
+  expected "To be able to turn user needs into actionable technical tasks that can be delivered incrementally", 
+    'We expect our engineering team to be able to turn ideas into technical tasks.'
+
+  expected "To be involved in improving the academy program for current and future cohorts", 
+    'We expect our Academy Engineers to actively improve the academy programme.'
+
+  expected "To keep timesheets up-to-date and submitted before invoicing deadlines", 
+    'We use timesheets to invoice our customers, and it is important that we are able to send accurate invoices to our customers. We expect you to keep your timesheets up-to-date and to have submitted them before any invoicing deadlines.'
+
+  expected "To continuously make proactive improvements to the way Made Tech operates by first getting advice from impacted people and making the change",
+    'We expect everyone to evolve the way Made Tech operates. By involving everyone who is impacted by a change, it is possible for everyone to make improvements.'
 end
