5cbd1626dfd87e4ea3762f1224b9a9e282caabc7
Luke Morton <lukemorton.dev@gmail.com>
2019-11-26 18:06:13 +0000

    Update Engineer role (#313)
    ## Why
    
    The description of the job was fairly out of date, and not as inclusive as it could be.
    
    ## What
    
    Craig, myself and wider tech team have reviewed and updated content.

--- roles/as_code/lib/engineer.rb
+++ roles/as_code/lib/engineer.rb
@@ -4,46 +4,33 @@ JobSpec::Role.definition 'Engineer' do
 
     ## What does the job entail?
 
-    We primarily write and deliver custom software to our customers. We build solutions in a variety of languages and platforms, historically Ruby on Rails, we tend to use React, JavaScript, Elixir, and Clojure these days. We will usually be responsible for setting up a customer’s infrastructure on AWS with tools like Ansible and Terraform though sometimes we opt for a Platform as a Service like Heroku. We make sure there is a Continuous Delivery pipeline running our automated test suites before deploying changes.
+    We primarily write and deliver custom software for the public sector. We work across central and local government, as well as in health, and our past lies in the technology startup world. Technical excellence for us isn’t about delivering to feature lists. We place a strong emphasis on outcome-based delivery; ensuring our customer’s goals are understood and achieved with the technology we deploy.
 
-    The software we build solves business problems. We’ve built software for karaoke, e-commerce, retail buying departments, accountants, photography studios, warehouses, supply chain companies, and ourselves. Typical projects will last 3-6 months, some customers work with us over longer periods but we like to mix up teams at around 6 months to keep things fresh.
+    Our teams have used Ruby with Rails and Sinatra, ES6 with React and Angular 2, C# with .NET Core. We don’t limit ourselves as a company and we expect all our Engineers to be keen on learning new technologies. Automation is important to our teams, so we make sure there is a CD pipeline set up to build, test, and release. Also, we will usually be responsible for setting up a customer’s infrastructure too! Such as on AWS, GCP or Azure using a tool like Terraform, though sometimes we opt for a Platform as a Service like Heroku.
 
     ## What experience are we looking for?
 
-    We’ll expect you to be proficient in at least two programming languages. This is usually something like Ruby/Python/PHP/Java/C# and then JavaScript. Everyone in the organisation comes from various language backgrounds and we see this as a strength of our offering, our teams can solve problems with open minds and across many platforms. This also makes for a great learning environment.
+    All of our Software Engineers are trained first as an [Academy Engineer](academy_engineer.md) within our 12 week Academy programme. We don't currently hire people into this role without them first going through our Academy.
 
-    Code is only the half of it. The other half is communication. We expect you to be a great communicator with customers and colleagues in written and spoken English. We meet our customers face to face regularly, even working from their office, along with keeping in touch via Slack and Google Hangouts.
+    ## What we will provide you
 
-    Everything else is optional but highly sought after. We would love you to have experience in:
-
-    - Previous experience working directly with customers
-    - Writing code with Test Driven Development or perhaps ATDD
-    - Mentoring colleagues providing them with guidance rather than solutions
-    - Pair programming as we pair around 50% of the time
-    - Agile practices like Scrum, XP, and/or Kanban
-    - Component based design techniques such as using pattern libraries, styled components, CSS-in-JS, BEM, and/or SUIT CSS
-    - React ecosystem including a TDD approach
-    - Working with a range of SQL and NoSQL databases
-    - Infrastructure as code tools like Ansible, Chef, Puppet, and/or Saltstack
-    - Strong delivery skills: a huge drive to get something shipped
-
-    ## What is it like to work at Made Tech?
-
-    We are a sanctuary for those wanting to hone their skills alongside like-minded learners. After joining our team it is common for new starters to comment on how much they’re learning and how much they enjoy the fact they are surrounded by people they can learn from. This includes our most senior hires.
-
-    The biggest thing you’ll take away from our culture after spending the day with us would be that we practice continuous improvement at every level. Everyone has peer-based one-to-ones every 2 weeks, along with monthly one-to-ones with a director. Teams have fortnightly retrospectives. We also hold a company wide retrospective fortnightly. We discuss our problems out in the open, and rather than punish failure we band together to find solutions.
-
-    Other notable things:
-
-    - Every Friday afternoon is dedicated to learning new skills
-    - Everyone is encouraged to write blog posts regularly
-    - Our handbook is open sourced
-    - We are vegan and non-drinker friendly as well as meat-eater and drinker friendly
-    - Retreats and trips every year
+    - Time to learn every Friday afternoon
+    - Space to write blog posts regularly
+    - Contribute to our culture and shape the way we work – even our handbook is open-sourced on Github
+    - Regular lunch and socials with colleagues – we are vegan, halal, and non-drinker friendly, as well as meat-eater and drinker friendly
+    - Retreats and trips with your colleagues every year
+    - Paid holidays for your third and fifth anniversary working for us
+    - Flexible holiday, working hours, and part-time
+    - Generous parental leave and support
+    - Season ticket loan
+    - Cycle to work scheme, a place to secure your bike and a shower
+    - Contributory pension scheme
+    - Conference tickets and training
+    - Volunteering time to help others get into the technology industry
 
     ## Salary
 
-    This role has a salary of £30-50k depending on experience.
+    This role has a salary of £30-50k.
   DESCRIPTION
 
   salary 30_000..50_000
